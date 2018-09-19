39a0c41f4715c75284890e314b2b2642fd1688a5
Cedric David <cedric.david@btopenworld.com>
2018-09-19 05:41:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
 
     operator = params[:operation]
     num1 = params[:number1].to_i
-    num2 = params[:number2].to_1
+    num2 = params[:number2].to_i
     case operator
     when "add"
       "#{num1 + num2}"
