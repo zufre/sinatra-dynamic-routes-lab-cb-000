3e9ab9a46a787089eebaf532b6d6a819a18ba9fb
Cedric David <cedric.david@btopenworld.com>
2018-09-19 05:18:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,11 @@ class App < Sinatra::Base
       phrase += "#{params[:phrase]}"
     end
     phrase
-
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] params[:word2] params[:word3] params[:word4] params[:word5]}"
+  end
 end
+
+  
