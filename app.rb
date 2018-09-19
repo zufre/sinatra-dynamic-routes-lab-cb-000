5143ac495fe7573a604b389be0219249948acf45
Cedric David <cedric.david@btopenworld.com>
2018-09-19 05:05:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    "#{parmas[:name].reverse}"
+    @reverseName = parmas[:name].reverse
+    "#{@reverseName}"
   end
 
 end
