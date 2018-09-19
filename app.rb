0266abf3de10c6e563b1c0a5740361174f4c987e
Cedric David <cedric.david@btopenworld.com>
2018-09-19 05:31:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,4 +23,17 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when "add "
+      "#{params[:number1] + params[:number2]}"
+    when "subtract"
+      "#{params[:number1] - params[:number2]}"
+    when "multiply"
+      "#{params[:number1] * params[:number2]}"
+    when "divide"
+      "#{params[:number1] / params[:number2]}"
+    end
+  end
 end
